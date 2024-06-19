a0a7a2b1fd43dd1e7b1ce253c69347ce01f1365f
sihyunjojo <si4018@naver.com>
2024-06-19 16:11:42 +0900

    feat: InsufficientBalanceException to include a custom message for insufficient balance.

--- src/main/java/org/c4marathon/assignment/exception/InsufficientBalanceException.java
+++ src/main/java/org/c4marathon/assignment/exception/InsufficientBalanceException.java
@@ -1,2 +1,7 @@
-package org.c4marathon.assignment.exception;public class InsufficientBalanceException {
+package org.c4marathon.assignment.exception;
+
+public class InsufficientBalanceException extends IllegalArgumentException{
+    public InsufficientBalanceException() {
+        super("잔액이 불충분합니다.");
+    }
 }
