6bd16b43f0f2fc8e28af8508b57a28a3a37960c7
sihyunjojo <si4018@naver.com>
2024-06-19 16:13:17 +0900

    feat: implement cacluateService
    Implemented methods to calculate and distribute balances equally and randomly among multiple accounts in the CalculateService class.

--- src/main/java/org/c4marathon/assignment/service/CalculateService.java
+++ src/main/java/org/c4marathon/assignment/service/CalculateService.java
@@ -1,2 +1,64 @@
-package org.c4marathon.assignment.service;public class CalculateService {
+package org.c4marathon.assignment.service;
+
+import lombok.RequiredArgsConstructor;
+import org.c4marathon.assignment.generator.RandomGenerator;
+import org.c4marathon.assignment.validate.AccountValidator;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.math.BigDecimal;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Random;
+
+import static org.c4marathon.assignment.util.AccountType.ADMIN_ACCOUNT_ID;
+
+@Service
+@RequiredArgsConstructor
+public class CalculateService {
+    private final AccountService accountService;
+    private final RandomGenerator randomGenerator;
+    private final AccountValidator accountValidator;
+    private final TransferService transferService;
+
+    @Transactional
+    public void calculateBalanceEqually(Long fromAccountId, List<Long> toAccountIds, BigDecimal totalBalance) {
+        accountValidator.validateDecimalBalance(totalBalance);
+
+        Map<Long, BigDecimal> equalBalances = distributeEqually(toAccountIds, totalBalance);
+
+        for (Map.Entry<Long, BigDecimal> entry : equalBalances.entrySet()) {
+            transferService.transferMoney(fromAccountId, entry.getKey(), entry.getValue());
+        }
+    }
+
+    private Map<Long, BigDecimal> distributeEqually(List<Long> toAccountIds, BigDecimal totalBalance) {
+        int numberOfAccounts = toAccountIds.size();
+        int totalBalanceInt = totalBalance.intValueExact();
+        int perAccountBalanceInt = totalBalanceInt / numberOfAccounts;
+        int remainderInt = totalBalanceInt % numberOfAccounts;
+
+        Map<Long, BigDecimal> accountBalances = new HashMap<>();
+        for (Long toAccountId : toAccountIds) {
+            BigDecimal amountToSend = BigDecimal.valueOf(perAccountBalanceInt);
+            accountBalances.put(toAccountId, amountToSend);
+        }
+
+        if (remainderInt > 0) {
+            BigDecimal remainder = BigDecimal.valueOf(remainderInt);
+            accountBalances.put(ADMIN_ACCOUNT_ID, remainder);
+        }
+
+        return accountBalances;
+    }
+
+
+    @Transactional
+    public void calculateBalanceRandomly(Long fromAccountId, List<Long> toAccountIds, BigDecimal totalBalance) {
+        Map<Long, BigDecimal> randomBalanceMap = randomGenerator.generateRandomBalances(toAccountIds, totalBalance);
+        for (Map.Entry<Long, BigDecimal> entry : randomBalanceMap.entrySet()) {
+            transferService.transferMoney(fromAccountId, entry.getKey(), entry.getValue());
+        }
+    }
 }
