441867c8ac96b6e79cc159bb35cceae10de816db
sihyunjojo <si4018@naver.com>
2024-06-20 00:24:21 +0900

    refactor: Refactor RechargeService to handle recharging logic more efficiently and accurately

--- src/main/java/org/c4marathon/assignment/service/RechargeService.java
+++ src/main/java/org/c4marathon/assignment/service/RechargeService.java
@@ -13,19 +13,19 @@ import java.math.BigDecimal;
 import static org.c4marathon.assignment.util.AccountType.RECHARGE_INCREMENT;
 
 @Service
-@RequiredArgsConstructor
 public class RechargeService {
-    private AccountRepository accountRepository;
 
     @Transactional(propagation = Propagation.REQUIRES_NEW, isolation = Isolation.REPEATABLE_READ)
     public void rechargeIncrements(BigDecimal requiredAmount, Account account) {
         BigDecimal currentBalance = account.getBalance();
         BigDecimal amountToRecharge = requiredAmount.subtract(currentBalance);
 
+        BigDecimal rechargeBalance = BigDecimal.ZERO;
         while (amountToRecharge.compareTo(BigDecimal.ZERO) > 0) {
-            account.setBalance(account.getBalance().add(RECHARGE_INCREMENT));
-            accountRepository.save(account);
+            // 추후 연결계좌가 주어진다면 연결계좌에 돈이 없을시 에러 발생시키고 기존에 가져간 돈 다시 돌려줘야함.
+            rechargeBalance = rechargeBalance.add(RECHARGE_INCREMENT);
             amountToRecharge = amountToRecharge.subtract(RECHARGE_INCREMENT);
         }
+        account.setBalance(account.getBalance().add(rechargeBalance));
     }
 }
