8bc284eb1bc7efd7d62d0c8db70719e4004c81cf
sihyunjojo <si4018@naver.com>
2024-06-20 01:44:37 +0900

    rename: Refactor DailyJobScheduler to ResetLimitScheduler and update method names accordingly. Add comments for clarity about the purpose of the method and the reset time for the limit.

--- src/main/java/org/c4marathon/assignment/config/scheduler/ResetLimitScheduler.java
+++ src/main/java/org/c4marathon/assignment/config/scheduler/ResetLimitScheduler.java
@@ -11,15 +11,16 @@ import java.time.LocalDateTime;
 
 @Component
 @RequiredArgsConstructor
-public class DailyJobScheduler {
+public class ResetLimitScheduler {
 
     private final JobLauncher jobLauncher; // 일괄 작업을 시작하는 데 사용됩니다.
     private final Job resetDailyLimitJob; // BatchConfig 클래스에 정의된 작업입니다.
 
 
+    // 인당 한도는 이제 0시 00분을 기점으로 초기화 되어야 합니다.
     // 매일 자정에 dailyResetJob 배치 작업이 실행되어 dailyResetTasklet에 정의된 작업을 수행하게 됩니다.
     @Scheduled(cron = "0 0 0 * * ?") // 매일 자정에 메서드가 실행되도록 예약합니다.
-    public void runResetDailyLimitJob() {
+    public void runResetLimitJob() {
         try {
             jobLauncher.run(resetDailyLimitJob, new JobParametersBuilder()
                     .addString("runTime", LocalDateTime.now().toString())
