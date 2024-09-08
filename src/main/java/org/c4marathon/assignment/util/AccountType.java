9d4174b17b2d0f95f81cf7fcaaae35fa03803670
sihyunjojo <si4018@naver.com>
2024-09-08 13:14:47 +0900

    refactor: 지정자 위치 변경

--- src/main/java/org/c4marathon/assignment/util/AccountType.java
+++ src/main/java/org/c4marathon/assignment/util/AccountType.java
@@ -1,26 +1,23 @@
 package org.c4marathon.assignment.util;
 
-import org.springframework.batch.core.repository.dao.JdbcStepExecutionDao;
-
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
 
 public class AccountType {
-    public final static String MAIN = "MAIN";
-    public final static String SAVINGS = "SAVINGS";
-    public final static String FLEXIBLE_SAVINGS = "FLEXIBLE_SAVINGS";
-    public final static String FLEXIBLE_SAVINGS_INTEREST_RATE = "0.03";
-    public final static String RECURRING_SAVINGS = "RECURRING_SAVINGS";
-    public final static String RECURRING_SAVINGS_INTEREST_RATE = "0.05";
-    public final static BigDecimal DAILY_CHARGING_LIMIT =  new BigDecimal("3000000");
-    public final static BigDecimal RECHARGE_INCREMENT =  new BigDecimal("10000");
+    public static final String MAIN = "MAIN";
+    public static final String SAVINGS = "SAVINGS";
+    public static final String FLEXIBLE_SAVINGS = "FLEXIBLE_SAVINGS";
+    public static final String FLEXIBLE_SAVINGS_INTEREST_RATE = "0.03";
+    public static final String RECURRING_SAVINGS = "RECURRING_SAVINGS";
+    public static final String RECURRING_SAVINGS_INTEREST_RATE = "0.05";
+    public static final BigDecimal DAILY_CHARGING_LIMIT =  new BigDecimal("3000000");
+    public static final BigDecimal RECHARGE_INCREMENT =  new BigDecimal("10000");
 
-    public final static String RANDOM_CALCULATE= "RANDOM_CALCULATE";
-    public final static String EQUALLY_CALCULATE= "EQUALLY_CALCULATE";
+    public static final String RANDOM_CALCULATE= "RANDOM_CALCULATE";
+    public static final String EQUALLY_CALCULATE= "EQUALLY_CALCULATE";
 
+    public static final LocalDateTime REMIND_STANDARD_TIME = LocalDateTime.now().plusHours(24);
 
-    public final static LocalDateTime REMIND_STANDARD_TIME = LocalDateTime.now().plusHours(24);
-
-    public final static long ADMIN_ACCOUNT_ID = 1L;
+    public static final long ADMIN_ACCOUNT_ID = 1L;
 
 }
